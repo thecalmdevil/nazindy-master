395ce67e9a1129764ab96ad824e5e1c1454f296e
TheCalmDevil <you@example.com>
2022-08-10 15:04:32 -0400

    1

--- components/SideNavbar.js
+++ components/SideNavbar.js
@@ -17,7 +17,7 @@ function SideNavbar() {
   return (
     <div>
       <Disclosure as="nav">
-        <Disclosure.Button className="absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-800 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white group">
+        <Disclosure.Button className="top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-800 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white group">
           <GiHamburgerMenu
             className="block md:hidden h-6 w-6"
             aria-hidden="true"
